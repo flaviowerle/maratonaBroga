09661b07344f3d42a75cf2cb1b1eaecb60d6c848
Flavio Werle <flavio.werle@gmail.com>
2020-07-01 18:12:04 -0300

    sequelize configured

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./models');
 
 const authController = require('./controllers/auth');
 
@@ -10,6 +11,8 @@ app.get('/',(req, res)=>{
     return res.json('Api running...');
 });
 
+db.sequelize.sync().then(()=>{
 app.listen(3001,()=>{
     console.log('Listening on port 3001');
+   });
 });
